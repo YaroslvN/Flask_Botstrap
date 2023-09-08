bb3ce9be072e3829d03108b420e3a8ee3e8d78ac
Yarosla Nyanyushkin <yaroslav.nyanyushin2005@gmail.com>
2023-09-08 09:57:50 +0400

    Initial commit

---
+++ FlaskPython.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template("Index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("About.html")
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return "User Page" + name + "-" + str(id)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
